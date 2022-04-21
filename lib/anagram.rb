8c620048728eb0a2aed5db419596f56885fd75fb
Yazmin Perez <zoeperez333@gmail.com>
2022-04-21 14:17:48 -0700

    update anagram_checker and spec. test 1 passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,35 @@
 require('pry')
 
 class Anagram
-  attr_accessor(:word1, :word2)
+  attr_accessor :first_input, :second_input
 
-  def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+  def initialize(first_input, second_input)
+    @first_input = first_input
+    @second_input = second_input
   end
 
   def anagram_checker()
-    if @word1 === @word2
-      true
-    else
-      false
-    end
+  @first_input = @first_input.chars
+  @second_input = @second_input.chars
+  [@first_input, @second_input]
   end
 end
+  
+
+
+
+  # second_input = @word2.each_char do |letter|
+  #    [letter]
+  # end
+      # want to compare each letter 
+    # if first_input.match?(second_input) === second_input
+    #   true
+    # else
+    #   false
+
+
+
+
+# create antigram method ex: tea, eat
+# creat vowel method? use include?
+#  >> append
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,14 +4,19 @@ require('anagram')
 
 describe(Anagram) do
   describe('#anagram_checker') do
-    it('return true if the words are the same ') do
-      input = Anagram.new('bye', 'bye')
-      expect(input.anagram_checker).to(eq(true))
-    end
+    # it('return true if the words are the same ') do
+    #   input = Anagram.new('bye', 'bye')
+    #   expect(input.anagram_checker).to(eq(true))
+    # end
 
-    it('return false if words are not the same') do
-      input = Anagram.new('bye', 'hi')
-      expect(input.anagram_checker).to(eq(false))
+    # it('return false if words are not the same') do
+    #   input = Anagram.new('bye', 'hi')
+    #   expect(input.anagram_checker).to(eq(false))
+    # end
+
+  it('return true if both words are anagrams') do
+    input = Anagram.new('hi', 'bye')
+    expect(input.anagram_checker).to(eq([['h', 'i'], ['b', 'y', 'e']]))
     end
   end
 end
