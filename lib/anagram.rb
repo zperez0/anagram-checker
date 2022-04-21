ec2b32a4df05aa7f3d0f41eb0cb8dcb6944463bd
Yazmin Perez <zoeperez333@gmail.com>
2022-04-21 11:55:05 -0700

    add Anagram class, initialize method. Update spec, test 1 passing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,18 @@
 require('pry')
 
-def anagram_checker()
+class Anagram
+  attr_accessor(:word1, :word2)
 
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+
+  def anagram_checker()
+    @word1 = 'bye'
+    @word2 = 'bye'
+    if @word1 === @word2
+      true
+    end
+  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,9 +2,11 @@ require('rspec')
 require('anagram')
 
 
-
+describe(Anagram) do
   describe('#anagram_checker') do
     it('return true if the words are the same ') do
-      expect(anagram_checker('bye', 'bye')).to(eq(true))
+      input = Anagram.new('bye', 'bye')
+      expect(input.anagram_checker).to(eq(true))
     end
   end
+end
